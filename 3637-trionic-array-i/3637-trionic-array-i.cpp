d4fbd727fd4c0bbe60c8979d58261a673ccd4a9a
aditya kesharwani <134911781+Riyansh-adi@users.noreply.github.com>
2026-02-03 13:12:32 +0530

    Time: 0 ms (100%), Space: 27.6 MB (96.31%) - LeetHub

--- 3637-trionic-array-i/3637-trionic-array-i.cpp
+++ 3637-trionic-array-i/3637-trionic-array-i.cpp
@@ -3,28 +3,20 @@ public:
     bool isTrionic(vector<int>& nums) {
         int n = nums.size();
         int i = 0;
-
-        // 1️⃣ Strictly increasing
         while (i + 1 < n && nums[i] < nums[i + 1]) {
             i++;
         }
-        if (i == 0) return false; // no increasing part
-
-        // 2️⃣ Strictly decreasing
+        if (i == 0) return false; 
         int p = i;
         while (i + 1 < n && nums[i] > nums[i + 1]) {
             i++;
         }
-        if (i == p) return false; // no decreasing part
-
-        // 3️⃣ Strictly increasing again
+        if (i == p) return false; 
         int q = i;
         while (i + 1 < n && nums[i] < nums[i + 1]) {
             i++;
         }
-        if (i == q) return false; // no final increasing part
-
-        // Must reach the end
+        if (i == q) return false; 
         return i == n - 1;
     }
 };
